33d318ee2e93f87c5439445656ca3ee0ed72faa2
alex <aleksnx@gmail.com>
2018-10-07 20:33:12 +0300

    homework theme 2

---
+++ theme_2.py
@@ -0,0 +1,5 @@
+games_list = {'Петров Петр': 'CSGo', 'Иванов Иван': 'Fifa 2019', 'Иванова Иванка': '2048'}
+print(games_list)
+del games_list['Иванова Иванка']
+games_list['Петров Петр'] = '2048'
+print(games_list)
