ee7ef78bfbced156e4455560a9dbc6f1f4031da0
alex <aleksnx@gmail.com>
2018-10-07 20:47:37 +0300

    homework theme 3 task 2

---
+++ theme_3_2.py
@@ -0,0 +1,26 @@
+import turtle
+
+t = turtle.Pen()
+
+t.forward(100)
+t.left(90)
+
+t.forward(86.6)
+t.left(90)
+
+t.forward(100)
+t.left(90)
+
+t.forward(86.6)
+t.left(90)
+
+t.forward(100)
+t.left(120)
+
+t.forward(100)
+t.left(120)
+
+t.forward(100)
+t.left(120)
+
+turtle.exitonclick()
